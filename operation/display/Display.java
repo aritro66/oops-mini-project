432085ba0b7d9472e1c66ba2c4c12675fc7087cd
aritro66 <ghosharitro66@gmail.com>
2021-11-17 15:49:29 +0530

    ch

--- operation/display/Display.java
+++ operation/display/Display.java
@@ -1,13 +1,23 @@
 package operation.display;
 import java.sql.*;
+import java.util.Formatter;
 
 public class Display {
     public static void displayAll(Connection con) throws SQLException {
         Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from members");
+            ResultSet rs = stmt.executeQuery("select * from menu");
             System.out.println("Working");
+            
             while (rs.next())
-                System.out.println( rs.getInt(1) + "  " + rs.getString(2) + "  "+rs.getString(3));
+            {
+                Formatter formatter = new Formatter();
+
+                // Use Space format specifier
+                formatter.format("%-4s%-35s%-25s%-15s%-15s",rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getInt(5));
+                // System.out.printf();
+                System.out.println(formatter);
+                // System.out.printf();
+            }
     }
     public static void displayCatagories(Connection con) throws SQLException {
         Statement stmt = con.createStatement();

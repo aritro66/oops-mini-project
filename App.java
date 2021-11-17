ba8a46549d908e2b322579d2ef1b7e78483f3f96
aritro66 <ghosharitro66@gmail.com>
2021-11-17 10:01:20 +0530

    loaddata

--- App.java
+++ App.java
@@ -1,4 +1,7 @@
+import java.io.*;
 import java.sql.*;
+import java.util.*;
+
 import operation.display.*;
 import operation.insert.*;
 import stats.count.*;
@@ -17,14 +20,61 @@ public class App {
     // }
     public static void connect() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/college2", "root", "admin");
-            // here sonoo is database name, root is username and password
-            // Statement stmt = con.createStatement();
-            // ResultSet rs = stmt.executeQuery("select * from emp");
-            // System.out.println("Working");
-            // while (rs.next())
-            //     System.out.println( rs.getString(1) + "  " + rs.getString(1) + "  "+rs.getInt(3));
-            
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dummy", "root", "mysql2002");
+        // here sonoo is database name, root is username and password
+        // Statement stmt = con.createStatement();
+        // ResultSet rs = stmt.executeQuery("select * from emp");
+        // System.out.println("Working");
+        // while (rs.next())
+        // System.out.println( rs.getString(1) + " " + rs.getString(1) + "
+        // "+rs.getInt(3));
+
+    }
+
+    public static void loadData() throws SQLException {
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("show tables");
+        System.out.println("Working");
+        while (rs.next()) {
+            if (rs.getString(1).toLowerCase().equals("menu")) {
+                System.out.println("found");
+                return;
+            }
+        }
+        String line = "";
+        String splitBy = ",";
+        try {
+            // parsing a CSV file into BufferedReader class constructor
+            BufferedReader br = new BufferedReader(new FileReader("Menu.csv"));
+            Statement stmt2 = con.createStatement();
+            String sql = "CREATE TABLE menu(FOOD_NAME varchar(40),CATEGORY varchar(20),VEG_NONVEG_NA varchar(20),PRICE int) ";
+
+            stmt2.executeUpdate(sql);
+            line = br.readLine();
+            while ((line = br.readLine()) != null) // returns a Boolean value
+            {
+                String[] employee = line.split(splitBy); // use comma as separator
+                // System.out.println(employee);
+                // Formatter formatter = new Formatter();
+
+                // // Use Space format specifier
+                // formatter.format("%-35s%-25s%-15s%-15s",employee[0],employee[1],employee[2],employee[3]);
+                // // System.out.printf();
+                // System.out.println(formatter);
+                String query = " insert into menu values (?, ?, ?, ?)";
+                PreparedStatement preparedStmt = con.prepareStatement(query);
+                preparedStmt.setString(1, employee[0]);
+                preparedStmt.setString(2, employee[1]);
+                preparedStmt.setString(3, employee[2]);
+                preparedStmt.setInt(4, Integer.parseInt(employee[3]));
+
+                // execute the preparedstatement
+                preparedStmt.execute();
+            }
+            System.out.println("table formed");
+        } catch (Exception e) {
+            System.out.println("Table could formed");
+        }
     }
 
     public static void disconnect() throws SQLException {
@@ -44,81 +94,82 @@ public class App {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Display obj = new Display();
         Insert obj2 = new Insert();
-        CountByCategory obj3=new CountByCategory();
-        CountByVeg obj4=new CountByVeg();
-        CountByNonVeg obj5=new CountByNonVeg();
-        Avg obj6=new Avg();
-
-
+        CountByCategory obj3 = new CountByCategory();
+        CountByVeg obj4 = new CountByVeg();
+        CountByNonVeg obj5 = new CountByNonVeg();
+        Avg obj6 = new Avg();
 
         System.out.println(args.length);
+        // connect();
+        // loadData();
+        // disconnect();
 
         switch (args[0]) {
         case "-d":
-            switch (args[1]) {
-            case "all":
-                connect();
-                obj.displayAll(con);
-                disconnect();
-                break;
-            case "cat":
-                connect();
-                // Display obj=new Display();
-                obj.displayCatagories(con);
-                disconnect();
-                break;
-            default:
-                printHelp();
-
-                break;
-            }
-            break;
-        case "-i":
-            connect();
-            obj2.insertRecord(con, args);
-            disconnect();
-            break;
-        case "-count":
-            switch (args[1]) {
-            
-            case "cat":
-                connect();
-                // Display obj=new Display();
-                obj3.countCatagory(con);
-                disconnect();
-                break;
-            case "veg":
-                connect();
-                // Display obj=new Display();
-                obj4.countVeg(con);
-                disconnect();
-                break;
-            case "nonveg":
-                connect();
-                // Display obj=new Display();
-                obj5.countNonVeg(con);
-                disconnect();
-                break;
-            default:
-                printHelp();
-
-                break;
-            }
-            break;
-        case "-avg":
-            connect();
-            // Display obj=new Display();
-            obj6.countAvg(con);
-            disconnect();
-        
+        switch (args[1]) {
+        case "all":
+        connect();
+        obj.displayAll(con);
+        disconnect();
         break;
-            
-        case "-h":
-            printHelp();
-            break;
+        case "cat":
+        connect();
+        // Display obj=new Display();
+        obj.displayCatagories(con);
+        disconnect();
+        break;
         default:
-            printHelp();
-            break;
+        printHelp();
+
+        break;
+        }
+        break;
+        case "-i":
+        connect();
+        obj2.insertRecord(con, args);
+        disconnect();
+        break;
+        case "-count":
+        switch (args[1]) {
+
+        case "cat":
+        connect();
+        // Display obj=new Display();
+        obj3.countCatagory(con);
+        disconnect();
+        break;
+        case "veg":
+        connect();
+        // Display obj=new Display();
+        obj4.countVeg(con);
+        disconnect();
+        break;
+        case "nonveg":
+        connect();
+        // Display obj=new Display();
+        obj5.countNonVeg(con);
+        disconnect();
+        break;
+        default:
+        printHelp();
+
+        break;
+        }
+        break;
+        case "-avg":
+        connect();
+        // Display obj=new Display();
+        obj6.countAvg(con);
+        disconnect();
+
+        break;
+
+        case "-h":
+        printHelp();
+        break;
+        default:
+        printHelp();
+        break;
         }
     }
 }

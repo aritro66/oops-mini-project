e1acf1c6c4b254c5e3e5c51124eba5273d183fd4
aritro66 <ghosharitro66@gmail.com>
2021-11-24 19:18:48 +0530

    Adding version

--- App.java
+++ App.java
@@ -13,20 +13,25 @@ public class App {
 
     public static Connection con = null;
 
-    
-
-    
-
     public static void printHelp() {
         System.out.println("Help for commands:");
-        String[] commands={"-h","-l","-s all","-s cat \"CATEGORY\"","-s type \"FOOD_TYPE\"","-s fl cat \"CATEGORY\"","-s fl foodname \"FOOD_NAME\"","-s ps cat \"CATEGORY\"","-s ps foodname \"FOOD_NAME\"","-s range -e \"PRICE\"","-s range -g \"PRICE\"","-s range -ge \"PRICE\"","-s range -l \"PRICE\"","-s range -le \"PRICE\"","-i \"Food_Name\" \"Category\" \"Food_Type\" \"Price\"","-count cat","-count veg","-count nonveg","-avg cat","-avg veg","-avg nonveg","-u cat \"CATEGORY\" \"PRICE\"","-u veg \"PRICE\"","-u nonveg \"PRICE\"","-u id \"FOOD_ID\" \"PRICE\"","-u all \"PRICE\"","-d \"FOOD_ID\"","-o create \"FOOD_ID\" \"QUANTITY\"","-o show","-o showmore","-o cone \"FOOD_ID\"","-o call"};
-        String[] description={"Help for commands","Load Menu.csv in MySql","Display whole menu","Display menu records for particular category","Display menu records for particular food type","Display menu records for particular category (searching by first letters)","Display menu records for particular Food name (searching by first letters)","Display menu records for particular category (searching by Partial Strings)","Display menu records for particular Food name (searching by Partial Strings)","Display menu records for given price","Display menu records for greater than given price","Display menu records for greater equal given price","Display menu records for lesser than given price","Display menu records for lesser equal given price","Insert new menu record with attribute Food_Name,Category,Food_Type,Price (Food Id is generated atomatically)","count number of records in each category","count number of records for veg food","count number of records for nonveg food","average number of records in each category","average number of records for veg food","average number of records for nonveg food","update menu records price by category","update menu records price of veg food","update menu records price of nonveg food","update menu record price for given food id","update price of whole menu","delete menu record by food id","Place order with food id and quantity","Show order list","Show order list with more information","Cancel particular order","cancel whole order"};
+        String[] commands={"-h","-l","-s all","-s cat \"CATEGORY\"","-s type \"FOOD_TYPE\"","-s fl cat \"CATEGORY\"","-s fl foodname \"FOOD_NAME\"","-s ps cat \"CATEGORY\"","-s ps foodname \"FOOD_NAME\"","-s range -e \"PRICE\"","-s range -g \"PRICE\"","-s range -ge \"PRICE\"","-s range -l \"PRICE\"","-s range -le \"PRICE\"","-i \"Food_Name\" \"Category\" \"Food_Type\" \"Price\"","-count cat","-count veg","-count nonveg","-avg cat","-avg veg","-avg nonveg","-u cat \"CATEGORY\" \"PRICE\"","-u veg \"PRICE\"","-u nonveg \"PRICE\"","-u id \"FOOD_ID\" \"PRICE\"","-u all \"PRICE\"","-d \"FOOD_ID\"","-o create \"FOOD_ID\" \"QUANTITY\"","-o show","-o showmore","-o cone \"FOOD_ID\"","-o call","-v"};
+        String[] description={"Help for commands","Load Menu.csv in MySql","Display whole menu","Display menu records for particular category","Display menu records for particular food type","Display menu records for particular category (searching by first letters)","Display menu records for particular Food name (searching by first letters)","Display menu records for particular category (searching by Partial Strings)","Display menu records for particular Food name (searching by Partial Strings)","Display menu records for given price","Display menu records for greater than given price","Display menu records for greater equal given price","Display menu records for lesser than given price","Display menu records for lesser equal given price","Insert new menu record with attribute Food_Name,Category,Food_Type,Price (Food Id is generated atomatically)","count number of records in each category","count number of records for veg food","count number of records for nonveg food","average number of records in each category","average number of records for veg food","average number of records for nonveg food","update menu records price by category","update menu records price of veg food","update menu records price of nonveg food","update menu record price for given food id","update price of whole menu","delete menu record by food id","Place order with food id and quantity","Show order list","Show order list with more information","Cancel particular order","cancel whole order","version"};
         
         for(int i=0;i<commands.length;i++)
         {
             System.out.printf("%-50s%s\n", commands[i],description[i]);
                 
         }
+        
+    }
+
+    public static void version() {
+        System.out.println("Version \"1.0.0\"");
+        System.out.println("Supports ->");
+        System.out.println("java version \"16.0.2\"");
+        System.out.println("mysql-connector-java \"8.0.27\"");
+        System.out.println("Lower versions may not work");
         
     }
 
@@ -277,6 +282,9 @@ public class App {
         break;
         case "-h":
             printHelp();
+            break;
+        case "-v":
+            version();
             break;
         default:
             printHelp();
